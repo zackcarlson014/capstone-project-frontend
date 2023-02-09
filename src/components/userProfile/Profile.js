c1b7d2875f6f87054b7666e67c99b78c6601a167
zackcarlson014 <jzcarlson014@gmail.com>
2023-02-09 12:45:57 -0600

    profile formatting refactor

--- src/components/userProfile/Profile.js
+++ src/components/userProfile/Profile.js
@@ -13,169 +13,193 @@ import { Grid, Button, Header, Icon, Loader } from 'semantic-ui-react';
 
 export class Profile extends Component {
 
-    //clear store state serachField before navigating away from component
-    componentWillUnmount() {
-        if (this.props.searchField) {
-            return this.props.clearSearch();
-        };
+  //clear store state serachField before navigating away from component
+  componentWillUnmount() {
+    if (this.props.searchField) {
+      return this.props.clearSearch();
     };
+  };
 
-    //list of all Library Books, searchField conditional results
-    // libraryBooks = () => {
-    //     if (this.props.searchField)  {
-    //         return this.props.allLibraryBooks.filter(b => 
-    //             b[0].title.toLowerCase().includes(this.props.searchField.toLowerCase()) 
-    //             || 
-    //             b[0].author.toLowerCase().includes(this.props.searchField.toLowerCase())
-    //         );
-    //     } else {
-    //         return this.props.allLibraryBooks;
-    //     };   
-    // };
+  //list of all Library Books, searchField conditional results
+  // libraryBooks = () => {
+  //     if (this.props.searchField)  {
+  //         return this.props.allLibraryBooks.filter(b => 
+  //             b[0].title.toLowerCase().includes(this.props.searchField.toLowerCase()) 
+  //             || 
+  //             b[0].author.toLowerCase().includes(this.props.searchField.toLowerCase())
+  //         );
+  //     } else {
+  //         return this.props.allLibraryBooks;
+  //     };   
+  // };
 
-    //list of all WishList Books, searchField conditional results
-    // wishedBooks = () => {
-    //     if (this.props.searchField)  {
-    //         return this.props.allWishedBooks.filter(b => 
-    //             b[0].title.toLowerCase().includes(this.props.searchField.toLowerCase()) 
-    //             || 
-    //             b[0].author.toLowerCase().includes(this.props.searchField.toLowerCase())
-    //         );
-    //     } else {
-    //         return this.props.allWishedBooks;
-    //     };   
-    // };
+  //list of all WishList Books, searchField conditional results
+  // wishedBooks = () => {
+  //     if (this.props.searchField)  {
+  //         return this.props.allWishedBooks.filter(b => 
+  //             b[0].title.toLowerCase().includes(this.props.searchField.toLowerCase()) 
+  //             || 
+  //             b[0].author.toLowerCase().includes(this.props.searchField.toLowerCase())
+  //         );
+  //     } else {
+  //         return this.props.allWishedBooks;
+  //     };   
+  // };
 
-    //number of my Library Books - prop for UserCard component
-    bookCount = () => {
-        let books = [];
-        books = this.props.allLibraryBooks.filter(book => 
-            book[1].id === this.props.auth.id
-        );
-        return books.length;
-    };
+  //number of my Library Books - prop for UserCard component
+  bookCount = () => {
+    let books = [];
+    books = this.props.allLibraryBooks.filter(book => 
+      book[1].id === this.props.auth.id
+    );
+    return books.length;
+  };
 
-    //my reserved books that have been delivered to me - prop for CurrentlyReadingCarousel component
-    deliveredBooks = () => {
-        const books = this.props.reservedBooks.filter(b => 
-            b.user_id === this.props.auth.id 
-            && 
-            b.delivered
-            &&
-            !b.completed
-        );
-        const libBooks = books.map(b => {
-            return this.props.allLibraryBooks.find(book => 
-                book[2] === b.user_lib_book_id 
-            );
-        });
-        return libBooks.map(b => b[0]);
-    };
+  //my reserved books that have been delivered to me - prop for CurrentlyReadingCarousel component
+  deliveredBooks = () => {
+    const books = this.props.reservedBooks.filter(b => 
+      b.user_id === this.props.auth.id 
+      && 
+      b.delivered
+      &&
+      !b.completed
+    );
+    const libBooks = books.map(b => {
+      return this.props.allLibraryBooks.find(book => 
+        book[2] === b.user_lib_book_id 
+      );
+    });
+    return libBooks.map(b => b[0]);
+  };
 
-    render() {
-        // window.scrollTo(0, 0);
-        if (!this.props.auth || this.props.reservedBooks === []) {
-            return <Grid style={{ height: '99vh' }}><Loader active /></Grid>;
-         } else {
-            return (
-                <div className='App'>
-                    <NavBar/><br/>
-                    <Grid>
-                        <Grid.Row></Grid.Row>
-                        <Grid.Row>
-                            <Grid.Column width='1'></Grid.Column>
-                            <Grid.Column width='10'>
-                                <UserCard user={this.props.auth} bookCount={this.bookCount()}/><br/><br/>
-                            </Grid.Column>
-                                {this.deliveredBooks().length !== 0 ?
-                                    <div>
-                                        <br/><br/><br/><CurrentlyReadingCarousel books={this.deliveredBooks()}/>
-                                    </div>
-                                    :
-                                    null
-                                }  
-                        </Grid.Row>
-                        <Grid.Row></Grid.Row>
-                    </Grid>
-                    <Header 
-                        as='h2' 
-                        icon={true}
-                        style={{color: 'white'}} 
-                        textAlign="center"
-                    >
-                        <Icon name='book' circular />
-                        <Header.Content>
-                            Your Library Books
-                        </Header.Content>
-                    </Header><br/>
-                    <Grid>
-                        <Grid.Row>
-                            <Grid.Column textAlign="center">
-                                <Button 
-                                    as={ Link } 
-                                    exact='true' 
-                                    to={'/user_lib_books/new'} 
-                                    animated='fade' 
-                                    icon={true} 
-                                    color='blue'
-                                >
-                                    <Button.Content visible>
-                                        <Icon name='book'/> 
-                                        +Library
-                                    </Button.Content>
-                                    <Button.Content hidden>
-                                        Search Books
-                                    </Button.Content>
-                                </Button>
-                            </Grid.Column>
-                        </Grid.Row>
-                        <Grid.Row></Grid.Row>
-                    </Grid>
-                    <LibraryBooks/><br/>
-                    <Header as='h2' icon style={{color: 'white'}} textAlign="center">
-                        <Icon name='book' circular />
-                        <Header.Content>
-                            Your WishList Books
-                        </Header.Content>
-                    </Header><br/>
-                    <Grid>
-                        <Grid.Row>
-                            <Grid.Column textAlign="center">
-                                <Button 
-                                    as={ Link } 
-                                    exact='true' 
-                                    to={'/user_wish_books/new'} 
-                                    animated='fade' 
-                                    icon={true} 
-                                    color='blue'
-                                >
-                                    <Button.Content visible>
-                                        <Icon name='book'/> 
-                                        +WishList
-                                    </Button.Content>
-                                    <Button.Content hidden>
-                                        Search Books
-                                    </Button.Content>
-                                </Button>
-                            </Grid.Column>
-                        </Grid.Row>
-                        <Grid.Row></Grid.Row>
-                    </Grid>
-                    <WishedBooks/><br/>
-                    <Footer/>
+  render() {
+    // window.scrollTo(0, 0);
+    if (!this.props.auth || this.props.reservedBooks === []) {
+      return <Grid style={{ height: '99vh' }}><Loader active /></Grid>;
+    } else {
+      return (
+        <div className='App'>
+          <NavBar/><br/>
+
+          <Grid>
+            <Grid.Row></Grid.Row>
+
+            <Grid.Row>
+              <Grid.Column width='1'></Grid.Column>
+
+              <Grid.Column width='10'>
+                <UserCard user={this.props.auth} bookCount={this.bookCount()}/><br/><br/>
+              </Grid.Column>
+
+              {this.deliveredBooks().length !== 0 ?
+                <div>
+                  <br/><br/><br/><CurrentlyReadingCarousel books={this.deliveredBooks()}/>
                 </div>
-            );
-        };
+                :
+                null
+              }  
+
+            </Grid.Row>
+
+            <Grid.Row></Grid.Row>
+          </Grid>
+
+          <Header 
+            as='h2' 
+            icon
+            style={{color: 'white'}} 
+            textAlign="center">
+
+            <Icon name='book' circular />
+
+            <Header.Content>
+              Your Library Books
+            </Header.Content>
+          </Header><br/>
+
+          <Grid>
+            <Grid.Row>
+              <Grid.Column textAlign="center">
+                <Button 
+                  as={ Link } 
+                  exact='true' 
+                  to={'/user_lib_books/new'} 
+                  animated='fade' 
+                  icon={true} 
+                  color='blue'>
+
+                  <Button.Content visible>
+                    <Icon name='book'/> 
+                    +Library
+                  </Button.Content>
+
+                  <Button.Content hidden>
+                    Search Books
+                  </Button.Content>
+                </Button>
+              </Grid.Column>
+            </Grid.Row>
+
+            <Grid.Row></Grid.Row>
+          </Grid>
+
+          <LibraryBooks/><br/>
+
+          <Header
+            as='h2'
+            icon
+            style={{color: 'white'}}
+            textAlign="center">
+    
+            <Icon name='book' circular />
+
+            <Header.Content>
+              Your WishList Books
+            </Header.Content>
+          </Header><br/>
+
+          <Grid>
+            <Grid.Row>
+              <Grid.Column textAlign="center">
+                <Button 
+                  as={ Link } 
+                  exact='true' 
+                  to={'/user_wish_books/new'} 
+                  animated='fade' 
+                  icon={true} 
+                  color='blue'>
+
+                  <Button.Content visible>
+                    <Icon name='book'/> 
+                    +WishList
+                  </Button.Content>
+
+                  <Button.Content hidden>
+                    Search Books
+                  </Button.Content>
+                </Button>
+              </Grid.Column>
+            </Grid.Row>
+
+            <Grid.Row></Grid.Row>
+          </Grid>
+
+          <WishedBooks/><br/>
+
+          <Footer/>
+        </div>
+      );
     };
+  };
 };
 
 const mapStateToProps = state => {
-    return {
-        auth: state.auth,
-        reservedBooks: state.reservedBooks,
-        allLibraryBooks: state.allLibraryBooks,
-        allWishedBooks: state.allWishedBooks
-    };
+  return {
+    auth: state.auth,
+    reservedBooks: state.reservedBooks,
+    allLibraryBooks: state.allLibraryBooks,
+    allWishedBooks: state.allWishedBooks
+  };
 };
 
 export default connect(mapStateToProps, { clearSearch })(Profile);
