176c9953fa9f17c42f37ab983f7eedac74cdcbc7
zackcarlson014 <jzcarlson014@gmail.com>
2023-08-03 22:49:03 -0500

    formatting for reserved message

--- src/components/reservedBooks/ReservedMessage.js
+++ src/components/reservedBooks/ReservedMessage.js
@@ -5,67 +5,109 @@ import { deleteReservedMessage } from '../../actions/index.js';
 import { Comment, Icon } from 'semantic-ui-react';
 
 export class ReservedMessage extends Component {
+  //delete message from frontend and backend
+  handleDeleteMessage = (e) => {
+    e.preventDefault();
 
-    //delete message from frontend and backend
-    handleDeleteMessage = (e) => {
-        e.preventDefault();
-        fetch(`http://localhost:3000/api/v1/reserved_messages/${this.props.message.id}`, {method: 'DELETE'})
-        .then(resp => resp.json())
-        .then(data => {
-            this.props.deleteReservedMessage(data.id);
-        });
+    const reqURL = `http://localhost:3000/api/v1/reserved_messages/${this.props.message.id}`;
+    
+    fetch(reqURL, {method: 'DELETE'})
+    .then(resp => resp.json())
+    .then(data => {
+      this.props.deleteReservedMessage(data.id);
+    });
+  };
+
+  //parse and format date/time 
+  dateTime = () => {
+    let period = 'am';
+
+    let hour = this.props.message.created_at.slice(11, 13); 
+    if (parseInt(hour) > 12) {
+        hour = String(parseInt(hour) - 12);
+        period = 'pm';
     };
 
-    //parse and format date/time 
-    dateTime = () => {
-        let period = 'am';
-        let hour = this.props.message.created_at.slice(11, 13); 
-        if (parseInt(hour) > 12) {
-            hour = String(parseInt(hour) - 12);
-            period = 'pm';
-        };
-        let minutes = this.props.message.created_at.slice(14,16);
-        let month = this.props.message.created_at.slice(5, 7);
-        if (month[0] === '0') {
-            month = month[1];
-        };
-        let day = this.props.message.created_at.slice(8, 10);
-        if (day[0] === '0') {
-            day = day[1];
-        };
-        let year = this.props.message.created_at.slice(2,4);
-        return `${hour}:${minutes} ${period} ${month}/${day}/${year}`;
+    const minutes = this.props.message.created_at.slice(14, 16);
+
+    let month = this.props.message.created_at.slice(5, 7);
+    if (month[0] === '0') {
+        month = month[1];
     };
 
-    render() {
-        return (
-            <div>
-                <Comment>
-                    <Comment.Avatar as={ Link } exact='true' to={this.props.user.id !== this.props.auth.id ? `/users/${this.props.user.id}` : '/profile'} src={this.props.user.prof_pic_url}/>
-                    <Comment.Content>
-                        <Comment.Author as={ Link } exact='true' to={this.props.user.id !== this.props.auth.id ? `/users/${this.props.user.id}` : '/profile'}>{this.props.user.username}</Comment.Author>
-                        <Comment.Metadata>
-                            <div>{this.dateTime()}</div>
-                        </Comment.Metadata>
-                        <Comment.Text>{this.props.message.content}</Comment.Text>
-                        {this.props.auth.id === this.props.user.id ?
-                            <Comment.Actions color='red'>
-                                <Comment.Action onClick={this.handleDeleteMessage} ><Icon name='trash alternate outline'/></Comment.Action>
-                            </Comment.Actions>
-                            :
-                            null
-                        }
-                    </Comment.Content>
-                </Comment>
-            </div>
-        );
+    let day = this.props.message.created_at.slice(8, 10);
+    if (day[0] === '0') {
+        day = day[1];
     };
+
+    const year = this.props.message.created_at.slice(2, 4);
+
+    return `${hour}:${minutes} ${period} ${month}/${day}/${year}`;
+  };
+
+  render() {
+    return (
+      <div>
+        <Comment>
+          <Comment.Avatar
+            as={ Link }
+            exact='true'
+            to={this.props.user.id !== this.props.auth.id
+              ? `/users/${this.props.user.id}`
+              : '/profile'
+            }
+            src={this.props.user.prof_pic_url}
+          />
+
+          <Comment.Content>
+            <Comment.Author
+              as={ Link }
+              exact='true'
+              to={this.props.user.id !== this.props.auth.id
+                ? `/users/${this.props.user.id}`
+                : '/profile'
+              }
+            >
+              {this.props.user.username}
+            </Comment.Author>
+
+            <Comment.Metadata>
+              <div>
+                {this.dateTime()}
+              </div>
+            </Comment.Metadata>
+
+            <Comment.Text>
+              {this.props.message.content}
+            </Comment.Text>
+
+            {this.props.auth.id === this.props.user.id
+              ? <Comment.Actions color='red'>
+                  <Comment.Action
+                    onClick={this.handleDeleteMessage}
+                  >
+                    <Icon name='trash alternate outline'/>
+                  </Comment.Action>
+                </Comment.Actions>
+
+              : null
+            }
+          </Comment.Content>
+        </Comment>
+      </div>
+    );
+  };
 };
 
 const mapStateToProps = state => {
-    return {
-        auth: state.auth
-    }
-}
+  return {
+    auth: state.auth,
+  };
+};
 
-export default connect(mapStateToProps, { deleteReservedMessage })(ReservedMessage);
+export default connect(
+  mapStateToProps,
+  { 
+    deleteReservedMessage,
+  },
+)(ReservedMessage);
