e72faff286fc438fc833143bec5fa2197ecd91f6
zackcarlson014 <jzcarlson014@gmail.com>
2023-08-04 07:12:40 -0500

    formatting for wished books

--- src/components/userProfile/WishedBooks.js
+++ src/components/userProfile/WishedBooks.js
@@ -1,55 +1,67 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import WishedBookCard from './WishedBookCard';
-import { Grid, Header, Pagination, Segment } from 'semantic-ui-react';
+import {
+  Grid,
+  Header,
+  Pagination,
+  Segment
+} from 'semantic-ui-react';
 
 export class WishedBooks extends Component {
-
   state = { 
     activePage: 1,
   };
 
   //matching serach returns initial activePage state value
   componentDidMount() {
-      this.setState({ 
-          activePage: 1
-      });
+    this.setState({ 
+      activePage: 1,
+    });
   }
 
   //set state's activePage to integer of page clicked
-  handlePaginationChange = (e, { activePage }) => {
-      this.setState({ 
-          activePage 
-      });
+  handlePaginationChange = (
+    e,
+    { activePage }
+  ) => {
+    this.setState({ 
+      activePage,
+    });
   };
 
   //amount of page buttons that should appear on pagination component
   paginationCount = () => {
-      return this.myWishedBooks().length / 8;
+    return this.myWishedBooks().length / 8;
   };
 
   //remove page if deleted book last on page
   deleteBookIndex = () => {
-      const updatedPage = this.state.activePage - 1;
-      if ((this.myWishedBooks().length / 8) < this.state.activePage && this.state.activePage > 1) {
-          this.setState({
-              activePage: updatedPage
-          });
-      };
+    const updatedPage = this.state.activePage - 1;
+
+    if (
+      (this.paginationCount) < this.state.activePage
+      && this.state.activePage > 1
+    ) {
+      this.setState({
+        activePage: updatedPage,
+      });
+    };
   };
 
   //all Wish List books that belong to current user
   myWishedBooks = () => {
     const myBooks = this.props.allWishedBooks.filter(book => 
       book[1].id === this.props.auth.id);
+
     if (this.props.searchField)  {
       return myBooks.filter(b => 
         b[0].title.toLowerCase().includes(this.props.searchField.toLowerCase()) 
         || b[0].author.toLowerCase().includes(this.props.searchField.toLowerCase())
       );
-    } else {
-      return myBooks;
-    }; 
+    }
+
+    return myBooks;
   };
 
   //all Library books that belong to users other than current user
@@ -63,6 +75,7 @@ export class WishedBooks extends Component {
     if (this.state.activePage === 1) {
       return this.state.activePage - 1;
     };
+
     return (this.state.activePage - 1) * 8;
   };
 
@@ -86,70 +99,75 @@ export class WishedBooks extends Component {
                         this.bookIndex(),
                         this.bookIndex() + 8
                       ).map((wishBook, i) => {
-
-                      if (this.libraryBooks()
-                        .find(b => 
-                          b[0].id === wishBook[0].id
-                        )) {
+                        if (this.libraryBooks()
+                          .find(b => 
+                            b[0].id === wishBook[0].id
+                          )) {
+                            return (
+                              <WishedBookCard 
+                                key={i} 
+                                book={wishBook[0]} 
+                                user={wishBook[1]} 
+                                userBookId={wishBook[2]} 
+                                deleteBookIndex={this.deleteBookIndex} 
+                                match={this.libraryMatch(wishBook)}
+                              />
+                            )
+                        } else {
                           return (
                             <WishedBookCard 
                               key={i} 
                               book={wishBook[0]} 
                               user={wishBook[1]} 
                               userBookId={wishBook[2]} 
-                              deleteBookIndex={this.deleteBookIndex} 
-                              match={this.libraryMatch(wishBook)}
+                              deleteBookIndex={this.deleteBookIndex}
                             />
                           )
-                      } else {
-                        return (
-                          <WishedBookCard 
-                            key={i} 
-                            book={wishBook[0]} 
-                            user={wishBook[1]} 
-                            userBookId={wishBook[2]} 
-                            deleteBookIndex={this.deleteBookIndex}
-                          />
-                        )
-                      }
-                  })}
-                </div>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
+                        }
+                    })}
+                  </div>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
           
           : <Grid textAlign='center'>
             <Grid.Row>
               <Grid.Column>
                 <Header as='h3' style={{color: 'white'}} textAlign="center">
-                  {this.props.searchField ? 
-                    "No Books Match Your Search" 
-                    : 
-                    "Your WishList Bookshelf is Current Empty"
+                  {this.props.searchField
+                    ? "No Books Match Your Search" 
+                    : "Your WishList Bookshelf is Current Empty"
                   }
                 </Header>
+
                 {this.props.searchField 
                   ? null  
-                  : <Header as='h4' style={{color: 'white'}} textAlign="center">
-                    Search for Books to add to you WishList
-                  </Header>
+                  : <Header
+                      as='h4'
+                      style={{color: 'white'}}
+                      textAlign="center"
+                    >
+                      Search for Books to add to you WishList
+                    </Header>
                 }
               </Grid.Column>
             </Grid.Row>
           </Grid>
         }
+
         <Grid textAlign='center'>
           <Grid.Row>
             <Segment color='blue' compact={true}>
               <Pagination 
                 activePage={this.state.activePage} 
-                onPageChange={this.handlePaginationChange} 
-                totalPages={this.paginationCount()} 
+                totalPages={this.paginationCount()}
+                onPageChange={this.handlePaginationChange}
               />
             </Segment>
           </Grid.Row>
-          <Grid.Row></Grid.Row>
-          <Grid.Row></Grid.Row>
+
+          <Grid.Row/>
+          <Grid.Row/>
         </Grid>
       </div>
     );
@@ -161,7 +179,7 @@ const mapStateToProps = state => {
     auth: state.auth,
     allWishedBooks: state.allWishedBooks,
     allLibraryBooks: state.allLibraryBooks,
-    searchField: state.searchField
+    searchField: state.searchField,
   };
 };
 
