816221c5f2632334db694c92e3755715a1928912
zackcarlson014 <jzcarlson014@gmail.com>
2023-08-03 20:15:54 -0500

    update formatting for friends and login components

--- src/components/friends/Friends.js
+++ src/components/friends/Friends.js
@@ -8,140 +8,179 @@ import Footer from '../Footer';
 import { Grid, Header, Loader, Icon } from 'semantic-ui-react';
 
 export class Friends extends Component {
+  state = {
+    users: [],
+  };
+  componentWillMount() {
+    fetch('http://localhost:3000/api/v1/users')
+    .then(resp => resp.json())
+    .then(users => {
+      this.setState({
+          users,
+      });
+    });
+  };
+  componentWillUnmount() {
+    this.props.clearSearch();
+  };
 
-    state = {
-        users: []
-    };
+  searchableUsers = () => {
+    let matchedUsers = this.state.users.filter(u =>
+      u.id !== this.props.auth.id
+    );
 
-    componentWillMount() {
-        fetch('http://localhost:3000/api/v1/users')
-        .then(resp => resp.json())
-        .then(users => {
-            this.setState({
-                users
-            });
-        });
-    };
+    if (this.props.searchField) {
+      const users = this.state.users.filter(u => 
+        u.username.toLowerCase()
+        .includes(this.props.searchField.toLowerCase())
+      );
 
-    componentWillUnmount() {
-        this.props.clearSearch()
+      if (users.length !== 0)
+        matchedUsers = users;
+
     }
 
-    searchableUsers = () => {
-        let matchedUsers = [];
-        const allUsers = this.state.users.filter(u => u.id !== this.props.auth.id);
-        if (this.props.searchField)  {
-            const users = this.state.users.filter(u => 
-                u.username.toLowerCase().includes(this.props.searchField.toLowerCase()));
-            if (users.length !== 0) {
-                matchedUsers = users;
-            } else {
-                matchedUsers = allUsers;
-            }; 
-        } else {
-            matchedUsers = allUsers;
-        };
-        return matchedUsers;
-    };
+    return matchedUsers;
+  };
 
-    currentUserFriends = () => {
-        let friends = [];
-        let friendIds = [];
-        friendIds = this.props.friends.filter(f => 
-            f.pending === false
-        ).map(f => {
-            return f.inviter_id === this.props.auth.id ? f.invitee_id : f.inviter_id;
-        });
-        friends = this.searchableUsers().filter(u => friendIds.includes(u.id));
-        return friends;
-    };
+  currentUserFriends = () => {
+    let friends = [];
+    let friendIds = [];
 
-    friendLibraryBooks = (id) => {
-        return this.props.allLibraryBooks.filter(b => b[1].id === id);
-    };
+    friendIds = this.props.friends
+    .filter(f => f.pending === false)
+    .map(f => {
+      return f.inviter_id === this.props.auth.id
+        ? f.invitee_id
+        : f.inviter_id;
+    });
 
-    pendingFriendRequestObjects = () => {
-        return this.props.friends.filter(f => 
-            f.pending === true 
-            && 
-            f.invitee_id === this.props.auth.id
-        ).map(f => {
-            return {
-                request: f,
-                user: this.state.users.find(u => u.id === f.inviter_id)
-            };
-        });
-    };
+    friends = this.searchableUsers().filter(u => 
+      friendIds.includes(u.id)
+    );
+    
+    return friends;
+  };
 
-    requestUsers = () => {
-        const userIds = this.props.friends.filter(f => 
-            f.invitee_id === this.props.auth.id && f.pending === true  
-        ).map(f => 
-            f.inviter_id
-        );
-        return this.state.users.filter(u => userIds.includes(u.id))
-    }
+  friendLibraryBooks = (id) => {
+    return this.props.allLibraryBooks.filter(b =>
+      b[1].id === id
+    );
+  };
 
-    render() {
-        window.scrollTo(0, 0);
-        if (!this.props.auth) {
-            return <Grid style={{ height: '80vh' }}><Loader active /></Grid>;
-        } else {
-            return (
-                <div className='App'>
-                    <NavBar/><br/>
-                    <Grid textAlign='center' style={this.pendingFriendRequestObjects().length !== 0 || this.currentUserFriends().length === 0 ? { minHeight: '99vh' } : null}>
-                        <Grid.Row></Grid.Row>
-                        <Grid.Row></Grid.Row>
-                        {this.pendingFriendRequestObjects().length !== 0 ?
-                            <Grid.Row>
-                                <Grid.Column width='10'>
-                                    <Grid.Row></Grid.Row>
-                                    <Grid.Row>
-                                        <Header as='h1' icon style={{color: 'white'}} textAlign='center'>
-                                            <Icon name='user' circular />
-                                            <Header.Content>
-                                                Pending Requests
-                                            </Header.Content>
-                                        </Header><br/>
-                                    </Grid.Row>
-                                    <Grid.Row></Grid.Row>
-                                    <Grid.Row>
-                                        <FriendRequests requestObjects={this.pendingFriendRequestObjects()}/><br/>
-                                    </Grid.Row>  
-                                    <Grid.Row></Grid.Row>
-                                </Grid.Column>
-                            </Grid.Row>
-                            :
-                            null
-                        }
-                        <Grid.Row>
-                            <Header as='h1' icon style={{color: 'white'}} textAlign='center'>
-                                <Icon name='user' circular />
-                                <Header.Content>
-                                    Your Friends
-                                </Header.Content>
-                            </Header>
-                        </Grid.Row>
-                        <Grid.Row></Grid.Row>
-                    </Grid><br/>
-                    {this.currentUserFriends().map(u =>
-                            <UserDash user={u} myBrarianLibraryBooks={this.friendLibraryBooks}/>   
-                        )}
-                    <Footer/>
-                </div>
-            );
-        };
+  pendingFriendRequestObjects = () => {
+    return this.props.friends.filter(f => 
+      f.pending === true 
+      && f.invitee_id === this.props.auth.id
+    ).map(f => {
+      return {
+        request: f,
+        user: this.state.users.find(u =>
+          u.id === f.inviter_id
+        )
+      };
+    });
+  };
+
+  requestUsers = () => {
+    const userIds = this.props.friends.filter(f => 
+      f.invitee_id === this.props.auth.id
+      && f.pending === true  
+    ).map(f => 
+      f.inviter_id
+    );
+
+    return this.state.users.filter(u =>
+      userIds.includes(u.id)
+    )
+  }
+
+  render() {
+    window.scrollTo(0, 0);
+
+    if (!this.props.auth) {
+      return (
+        <Grid style={{ height: '80vh' }}>
+          <Loader active />
+        </Grid>
+      );
+    } else {
+      return (
+        <div className='App'>
+          <NavBar/><br/>
+          <Grid textAlign='center' style={this.pendingFriendRequestObjects().length !== 0 || this.currentUserFriends().length === 0 ? { minHeight: '99vh' } : null}>
+            <Grid.Row/>
+
+            <Grid.Row/>
+
+            {this.pendingFriendRequestObjects().length === 0
+              ? null
+              : <Grid.Row>
+                  <Grid.Column width='10'>
+                      <Grid.Row></Grid.Row>
+                      <Grid.Row>
+                          <Header as='h1' icon style={{color: 'white'}} textAlign='center'>
+                              <Icon name='user' circular />
+                              <Header.Content>
+                                  Pending Requests
+                              </Header.Content>
+                          </Header><br/>
+                      </Grid.Row>
+                      <Grid.Row></Grid.Row>
+                      <Grid.Row>
+                          <FriendRequests requestObjects={this.pendingFriendRequestObjects()}/><br/>
+                      </Grid.Row>  
+                      <Grid.Row></Grid.Row>
+                  </Grid.Column>
+                </Grid.Row>
+            }
+
+            <Grid.Row>
+              <Header
+                as='h1'
+                icon
+                style={{color: 'white'}}
+                textAlign='center'
+              >
+                <Icon name='user' circular />
+                <Header.Content>
+                  Your Friends
+                </Header.Content>
+              </Header>
+            </Grid.Row>
+
+            <Grid.Row/>
+          </Grid>
+          
+          <br/>
+
+          {this.currentUserFriends().map((u, i) =>
+            <UserDash
+              key={i}
+              user={u}
+              myBrarianLibraryBooks={this.friendLibraryBooks}
+            />   
+          )}
+
+          <Footer/>
+        </div>
+      );
     };
+  };
 };
 
 const mapStateToProps = state => {
-    return {
-        auth: state.auth,
-        allLibraryBooks: state.allLibraryBooks,
-        friends: state.friends,
-        searchField: state.searchField
-    };
+  return {
+    auth: state.auth,
+    allLibraryBooks: state.allLibraryBooks,
+    friends: state.friends,
+    searchField: state.searchField,
+  };
 };
 
-export default connect(mapStateToProps, { clearSearch })(Friends);
+export default connect(
+  mapStateToProps,
+  {
+    clearSearch,
+  },
+)(Friends);
--- src/components/loginSignUp/Login.js
+++ src/components/loginSignUp/Login.js
@@ -6,79 +6,128 @@ import Footer from '../Footer';
 import { Button, Form, Grid, Header, Segment, Message, Icon } from 'semantic-ui-react';
 
 export class Login extends Component {
+  state = {
+    username: '',
+    password: '',
+    error: null,
+  };
 
-    state = {
-        username: '',
-        password: '',
-        error: null
+  handleInputChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const reqObj = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(this.state),
     };
 
-    handleInputChange = (e) => {
+    fetch('http://localhost:3000/api/v1/auth', reqObj)
+    .then(resp => resp.json())
+    .then(data => {
+      if (data.error) {
         this.setState({
-            [e.target.name]: e.target.value
+          error: data.error,
         });
-    };
+      } else {
+        this.props.loginSuccess(data);
+        this.props.history.push('/profile');
+        localStorage.setItem('my_app_token', data.token);
+      };
+    });
+  };
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        const reqObj = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(this.state)
-        };
-        fetch('http://localhost:3000/api/v1/auth', reqObj)
-        .then(resp => resp.json())
-        .then(data => {
-            if (data.error) {
-                this.setState({
-                    error: data.error
-                });
-            } else {
-                this.props.loginSuccess(data);
-                this.props.history.push('/profile');
-                localStorage.setItem('my_app_token', data.token);
-            };
-        });
-    };
+  render() {
+    return (
+      <div className='App'>
+        <Grid
+          textAlign='center'
+          style={{ height: '99vh' }}
+          verticalAlign='middle'
+        >
+          <Grid.Column style={{ maxWidth: 450 }}>
+            {this.state.error
+              ? <Header
+                  as='h4'
+                  block
+                  style={{color: 'red'}}
+                  textAlign='center'
+                >
+                  <Icon name='user'/>
+                  {this.state.error}
+                </Header>
+            
+              : null
+            }
 
-    render() {
-        return (
-            <div className='App'>
-                <Grid textAlign='center' style={{ height: '99vh' }} verticalAlign='middle'>
-                    <Grid.Column style={{ maxWidth: 450 }}>
-                        {this.state.error ? <Header as='h4' block style={{color: 'red'}} textAlign='center'><Icon name='user'/>{this.state.error}</Header> : null}
-                        <Header as='h2' icon style={{color: 'white'}} textAlign='center'>
-                            <Icon name='book' circular />
-                            <Header.Content>Welcome to MyBrary</Header.Content>
-                        </Header>
-                        <Form size='large' onSubmit={this.handleSubmit}>
-                            <Segment stacked>
-                            <Form.Input onChange={this.handleInputChange} icon='user' name='username' value={this.state.username} iconPosition='left' placeholder='Username' />
-                            <Form.Input
-                                onChange={this.handleInputChange}
-                                icon='lock'
-                                iconPosition='left'
-                                placeholder='Password'
-                                name='password'
-                                value={this.state.password}
-                                type='password'
-                            />
-                            <Button color='blue' fluid size='large'>
-                                Login
-                            </Button>
-                            </Segment>
-                        </Form>
-                        <Message>
-                            New to us? <Button as={ Link } exact='true' to={`/users/new`} color='green' size='mini'>Sign Up</Button>
-                        </Message>
-                    </Grid.Column>
-                </Grid>
-                <Footer/>
-            </div>
-        );
-    };
+            <Header
+              as='h2'
+              icon
+              style={{color: 'white'}}
+              textAlign='center'
+            >
+              <Icon name='book' circular />
+              <Header.Content>
+                Welcome to MyBrary
+              </Header.Content>
+            </Header>
+
+            <Form size='large' onSubmit={this.handleSubmit}>
+              <Segment stacked>
+                <Form.Input
+                  icon='user' name='username'
+                  value={this.state.username}
+                  iconPosition='left'
+                  placeholder='Username'
+                  onChange={this.handleInputChange}
+                />
+
+                <Form.Input
+                  icon='lock'
+                  iconPosition='left'
+                  placeholder='Password'
+                  name='password'
+                  value={this.state.password}
+                  type='password'
+                  onChange={this.handleInputChange}
+                />
+
+                <Button color='blue' fluid size='large'>
+                  Login
+                </Button>
+              </Segment>
+            </Form>
+
+            <Message>
+                New to us?
+                <Button
+                  as={ Link }
+                  exact='true'
+                  to={`/users/new`}
+                  color='green'
+                  size='mini'>
+                    Sign Up
+                </Button>
+            </Message>
+          </Grid.Column>
+        </Grid>
+
+        <Footer/>
+      </div>
+    );
+  };
 };
 
-export default connect(null, { loginSuccess })(Login);
+export default connect(
+  null,
+  {
+    loginSuccess,
+  },
+)(Login);
