32dc0819dc536e8daa0d389c56d9247396fc65dc
zackcarlson014 <jzcarlson014@gmail.com>
2023-08-04 07:16:54 -0500

    formatting for new message form

--- src/components/messages/NewMessageForm.js
+++ src/components/messages/NewMessageForm.js
@@ -1,35 +1,54 @@
 import React, { Component } from 'react';
-import { Grid, Form, Header, Icon, Button } from 'semantic-ui-react';
+import {
+  Grid,
+  Form,
+  Header,
+  Icon,
+  Button,
+} from 'semantic-ui-react';
 
 export class NewMessageForm extends Component {
-    render() {
-        return (
-            <div>
-                <Grid style={{ height: '99vh' }}>
-                    <Grid.Row></Grid.Row>
-                    <Grid.Row verticalAlign='middle'>
-                        <Grid.Column width='7'></Grid.Column>
-                        <Grid.Column width='2'>
-                            <Header as='h2' icon style={{color: 'white'}} textAlign="center">
-                                <Icon name='mail' circular />
-                                <Header.Content>
-                                    New Message
-                                </Header.Content>
-                            </Header>
-                        </Grid.Column>
-                        <Grid.Column width='7' textAlign='center'><Button icon='mail' content='new' color='blue'/></Grid.Column>
-                    </Grid.Row>
-                    <Grid.Row>
-                        <Form>
-                            <Form.Input>
+  render() {
+    return (
+      <div>
+        <Grid style={{ height: '99vh' }}>
+            <Grid.Row/>
+            
+            <Grid.Row verticalAlign='middle'>
+                <Grid.Column width='7'/>
 
-                            </Form.Input>
-                        </Form>
-                    </Grid.Row>
-                </Grid>
-            </div>
-        );
-    };
+                <Grid.Column width='2'>
+                  <Header
+                    as='h2'
+                    icon style={{color: 'white'}}
+                    textAlign="center"
+                  >
+                    <Icon name='mail' circular />
+                    <Header.Content>
+                      New Message
+                    </Header.Content>
+                  </Header>
+                </Grid.Column>
+
+                <Grid.Column width='7' textAlign='center'>
+                  <Button
+                    icon='mail'
+                    content='new'
+                    color='blue'
+                  />
+                </Grid.Column>
+            </Grid.Row>
+
+            <Grid.Row>
+              <Form>
+                <Form.Input>
+                </Form.Input>
+              </Form>
+            </Grid.Row>
+        </Grid>
+      </div>
+    );
+  };
 };
 
 export default NewMessageForm;
