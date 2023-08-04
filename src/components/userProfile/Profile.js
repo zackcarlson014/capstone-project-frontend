053f4c0232427198c5eb051680a399295e376829
zackcarlson014 <jzcarlson014@gmail.com>
2023-08-04 06:52:51 -0500

    formatting for profile

--- src/components/userProfile/Profile.js
+++ src/components/userProfile/Profile.js
@@ -8,11 +8,15 @@ import CurrentlyReadingCarousel from '../carousels/CurrentlyReadingCarousel';
 import LibraryBooks from './LibraryBooks';
 import WishedBooks from './WishedBooks';
 import Footer from '../Footer';
-import { Grid, Button, Header, Icon, Loader } from 'semantic-ui-react';
-
+import {
+  Grid,
+  Button,
+  Header,
+  Icon,
+  Loader
+} from 'semantic-ui-react';
 
 export class Profile extends Component {
-
   //clear store state serachField before navigating away from component
   componentWillUnmount() {
     if (this.props.searchField) {
@@ -48,10 +52,10 @@ export class Profile extends Component {
 
   //number of my Library Books - prop for UserCard component
   bookCount = () => {
-    let books = [];
-    books = this.props.allLibraryBooks.filter(book => 
+    const books = this.props.allLibraryBooks.filter(book => 
       book[1].id === this.props.auth.id
     );
+
     return books.length;
   };
 
@@ -59,63 +63,78 @@ export class Profile extends Component {
   deliveredBooks = () => {
     const books = this.props.reservedBooks.filter(b => 
       b.user_id === this.props.auth.id 
-      && 
-      b.delivered
-      &&
-      !b.completed
+      && b.delivered
+      && !b.completed
     );
+
     const libBooks = books.map(b => {
       return this.props.allLibraryBooks.find(book => 
         book[2] === b.user_lib_book_id 
       );
     });
+
     return libBooks.map(b => b[0]);
   };
 
   render() {
     // window.scrollTo(0, 0);
     if (!this.props.auth || this.props.reservedBooks === []) {
-      return <Grid style={{ height: '99vh' }}><Loader active /></Grid>;
+      return (
+        <Grid style={{ height: '99vh' }}>
+          <Loader active />
+        </Grid>
+      );
     } else {
       return (
         <div className='App'>
-          <NavBar/><br/>
+          <NavBar/>
+          
+          <br/>
 
           <Grid>
-            <Grid.Row></Grid.Row>
+            <Grid.Row/>
 
             <Grid.Row>
-              <Grid.Column width='1'></Grid.Column>
+              <Grid.Column width='1'/>
 
               <Grid.Column width='10'>
-                <UserCard user={this.props.auth} bookCount={this.bookCount()}/><br/><br/>
+                <UserCard
+                  user={this.props.auth}
+                  bookCount={this.bookCount()}
+                />
+                
+                <br/><br/>
               </Grid.Column>
 
-              {this.deliveredBooks().length !== 0 ?
-                <div>
-                  <br/><br/><br/><CurrentlyReadingCarousel books={this.deliveredBooks()}/>
-                </div>
-                :
-                null
+              {this.deliveredBooks().length !== 0
+                ? <div>
+                    <br/><br/><br/>
+                    <CurrentlyReadingCarousel
+                      books={this.deliveredBooks()}
+                    />
+                  </div>
+                
+                : null
               }  
 
             </Grid.Row>
 
-            <Grid.Row></Grid.Row>
+            <Grid.Row/>
           </Grid>
 
           <Header 
             as='h2' 
             icon
             style={{color: 'white'}} 
-            textAlign="center">
-
+            textAlign="center"
+          >
             <Icon name='book' circular />
-
             <Header.Content>
               Your Library Books
             </Header.Content>
-          </Header><br/>
+          </Header>
+          
+          <br/>
 
           <Grid>
             <Grid.Row>
@@ -126,8 +145,8 @@ export class Profile extends Component {
                   to={'/user_lib_books/new'} 
                   animated='fade' 
                   icon={true} 
-                  color='blue'>
-
+                  color='blue'
+                >
                   <Button.Content visible>
                     <Icon name='book'/> 
                     +Library
@@ -140,23 +159,26 @@ export class Profile extends Component {
               </Grid.Column>
             </Grid.Row>
 
-            <Grid.Row></Grid.Row>
+            <Grid.Row/>
           </Grid>
 
-          <LibraryBooks/><br/>
+          <LibraryBooks/>
+          
+          <br/>
 
           <Header
             as='h2'
             icon
             style={{color: 'white'}}
-            textAlign="center">
-    
+            textAlign="center"
+          >
             <Icon name='book' circular />
-
             <Header.Content>
               Your WishList Books
             </Header.Content>
-          </Header><br/>
+          </Header>
+          
+          <br/>
 
           <Grid>
             <Grid.Row>
@@ -167,8 +189,8 @@ export class Profile extends Component {
                   to={'/user_wish_books/new'} 
                   animated='fade' 
                   icon={true} 
-                  color='blue'>
-
+                  color='blue'
+                >
                   <Button.Content visible>
                     <Icon name='book'/> 
                     +WishList
@@ -181,10 +203,12 @@ export class Profile extends Component {
               </Grid.Column>
             </Grid.Row>
 
-            <Grid.Row></Grid.Row>
+            <Grid.Row/>
           </Grid>
 
-          <WishedBooks/><br/>
+          <WishedBooks/>
+          
+          <br/>
 
           <Footer/>
         </div>
@@ -198,7 +222,7 @@ const mapStateToProps = state => {
     auth: state.auth,
     reservedBooks: state.reservedBooks,
     allLibraryBooks: state.allLibraryBooks,
-    allWishedBooks: state.allWishedBooks
+    allWishedBooks: state.allWishedBooks,
   };
 };
 
